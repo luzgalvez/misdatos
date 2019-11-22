c9e3429d8347e41c6673511e955fea52849c2056
luzgalvez <luz.galvez@pachuca.gob.mx>
2019-11-21 18:19:27 -0600

    jkjk

---
+++ app/src/main/java/com/example/misdatos/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.misdatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+public Button nuevoboton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nuevoboton = findViewById(R.id.button3);
+
+        nuevoboton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void  onClick(View view){
+                Intent intent = new Intent(MainActivity.this, datos.class);
+                startActivity(intent);
+
+                Toast.makeText(getApplicationContext(),"Hoola",Toast.LENGTH_LONG).show();
+
+            }
+
+        });
+    }
+
+
+    public void CambiarActivity(View view){
+        Intent miIntent=null;
+        switch (view.getId()){
+            case R.id.misDatos:
+                Toast.makeText(this, "CambiarActivity", Toast.LENGTH_LONG).show();
+                miIntent=new Intent(MainActivity.this,datos.class);
+                if (miIntent != null) {
+                    startActivity(miIntent);
+                }
+                break;
+
+        }
+
+
+    }
+}
