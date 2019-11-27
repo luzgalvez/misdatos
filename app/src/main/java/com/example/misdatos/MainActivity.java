13d28d7e0295789855826ba0d5772c52b306e7d4
luzgalvez <luz.galvez@pachuca.gob.mx>
2019-11-26 18:33:48 -0600

    fracmentos con menu

--- app/src/main/java/com/example/misdatos/MainActivity.java
+++ app/src/main/java/com/example/misdatos/MainActivity.java
@@ -1,50 +1,86 @@
 package com.example.misdatos;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-public Button nuevoboton;
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        nuevoboton = findViewById(R.id.button3);
-
-        nuevoboton.setOnClickListener(new View.OnClickListener(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void  onClick(View view){
-                Intent intent = new Intent(MainActivity.this, datos.class);
-                startActivity(intent);
-
-                Toast.makeText(getApplicationContext(),"Hoola",Toast.LENGTH_LONG).show();
-
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-
         });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        sharedPreferences=getSharedPreferences("misDatos", Context.MODE_PRIVATE);
+
+        String pNombre=sharedPreferences.getString("nombre","reporte ciudadano");
+        String pemail=sharedPreferences.getString("email", "sin correo");
+
+        View headerView = navigationView.getHeaderView(0);
+
+        TextView headerNombre=headerView.findViewById(R.id.headerNombre);
+        TextView headerMail=headerView.findViewById(R.id.headerMail);
+
+        headerNombre.setText(pNombre);
+        headerMail.setText(pemail);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-    public void CambiarActivity(View view){
-        Intent miIntent=null;
-        switch (view.getId()){
-            case R.id.misDatos:
-                Toast.makeText(this, "CambiarActivity", Toast.LENGTH_LONG).show();
-                miIntent=new Intent(MainActivity.this,datos.class);
-                if (miIntent != null) {
-                    startActivity(miIntent);
-                }
-                break;
-
-        }
-
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
